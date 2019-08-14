65033af78aec07d2b318239ba5c887955a0594bc
nancyho629 <nancyho629@gmail.com>
2019-08-14 19:48:36 -0400

    Edit create api Line
    Added data to api create

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -38,13 +38,14 @@ const signOut = function () {
   })
 }
 
-const resetGame = function () {
+const resetGame = function (data) {
   return $.ajax({
     url: config.apiUrl + '/games',
     method: 'POST',
     headers: {
       Authorization: 'Token token=' + store.user.token
-    }
+    },
+    data
   })
 }
 
